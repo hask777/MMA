60579c28d6f077d91d05cf9c1ffae2843b6bce87
hask777 <haskellisp@gmail.com>
2021-02-28 22:14:56 +0400

    ff

--- resources/views/fighters.blade.php
+++ resources/views/fighters.blade.php
@@ -1,9 +1,19 @@
 @extends('main')
 @section('content')
+
     @foreach($fighters as $fighter)
-    <div class="">
-        <a href="{{route('fighter', [$fighter['FighterId']])}}">{{ $fighter['FirstName'] }}||{{ $fighter['LastName'] }}</a>
-        
-    </div>
+
+        <div class="bg-white mt-4 mb-4 p-4 flex justify-between">
+            <div>
+                <a href="{{route('fighter', [$fighter['FighterId']])}}">{{ $fighter['FirstName'] }}||{{ $fighter['LastName'] }}</a> 
+            </div>
+            
+
+            <div class="fighter_img">
+                @include('parts.user')
+            </div>
+        </div>
+
     @endforeach
+
 @endsection 
