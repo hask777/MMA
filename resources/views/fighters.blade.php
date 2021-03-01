b8d47a5ad97838ca46f904decde264bfe6369f6a
hask777 <haskellisp@gmail.com>
2021-03-01 15:36:27 +0400

    ff

--- resources/views/fighters.blade.php
+++ resources/views/fighters.blade.php
@@ -3,10 +3,27 @@
 
     @foreach($fighters as $fighter)
 
-        <div class="bg-white mt-4 mb-4 p-4 flex justify-between">
-            <div>
-                <a href="{{route('fighter', [$fighter['FighterId']])}}">{{ $fighter['FirstName'] }}||{{ $fighter['LastName'] }}</a> 
+    <a href="{{route('fighter', [$fighter['FighterId']])}}">
+        <div class="bg-white mt-4 mb-4 p-2">
+                    
+            <h1 class="font-bold text-2xl ">{{ $fighter['FirstName'] }}  {{ $fighter['LastName'] }} 
+                <span class="ml-4 text-red-500">{{ $fighter['Nickname'] }}</span>
+                <span class="text-xl ml-10">{{ $fighter['WeightClass'] }}</span>
+            </h1>
+            
+            <div class="flex justify-between">
+                <div class="stat">
+                   <div class="weight">birth date:{{ $fighter['BirthDate'] }}</div>
+                   <div class="weight">weight:{{ $fighter['Weight'] }}</div>
+                   <div class="height">height:{{ $fighter['Height'] }}</div>
+                   <div class="height">reach:{{ $fighter['Reach'] }}</div>
+                </div>
+                <div class="fighter_img w-32">
+                    {{-- @include('parts.user') --}}
+                    <img class="" src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2019-01/STEWART_KYLE_L.png?o5dKYwN7nm32idpAVQEBAHegkSJpt3sK&itok=H2pycD1D" alt="">
+                </div>
             </div>
+            
             {{-- NICNAME --}}
             {{-- WEIGHT --}}
             {{-- BirthDate  --}}
@@ -14,7 +31,7 @@
             {{-- Weight --}}
             {{-- Rich --}}
             {{-- Wins --}}
-            {{-- Losess --}}
+            {{-- Losess --}} 
             {{-- Draws --}}
             {{-- TechnicalKnockouts --}}
             {{-- TechnicalKnockoutLosses --}}
@@ -25,10 +42,9 @@
             {{-- TitleDraws --}}
             {{-- CareerStats --}}
 
-            <div class="fighter_img">
-                @include('parts.user')
-            </div>
-        </div>
+            
+        </div> 
+    </a>
 
     @endforeach
 
